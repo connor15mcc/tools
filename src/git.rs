93bf2868a6ef8ccaaf28b6fe3c32fac5354dc5f7
Connor McCarthy <connor15mcc@gmail.com>
2025-01-31 20:42:00 -0500

    wip: add pmr command for dealing with not-monorepo

--- src/git.rs
+++ src/git.rs
@@ -1,10 +1,14 @@
-use std::process::Command;
-
-use anyhow::{Context, Result};
+use std::{io::BufReader, process::Command};
+use anyhow::{Context as _, Result};
 use chrono::naive::NaiveDate;
+use std::io::BufRead;
+use std::io::Read;
 use futures::TryStreamExt;
-use git2::{build::CheckoutBuilder, IntoCString, Repository};
+use git2::{build::CheckoutBuilder, Repository};
 use tokio::pin;
+use xshell::{cmd, Shell};
+
+use crate::PrInfo;
 
 #[derive(Debug)]
 struct PullRequest {
@@ -119,4 +123,88 @@ pub fn tidy_merged_go_mod() -> Result<()> {
         .output()?;
 
     Ok(())
+}
+
+pub fn poor_mans_refactorator(id: &str, pr_info: Option<&PrInfo>, cmd: &str, repos: BufReader<impl Read>) -> Result<()> {
+    let dir = &format!(".run-{}", id);
+    let sh = Shell::new()?;
+    if !sh.path_exists(dir) {
+        sh.create_dir(dir)?;
+    }
+    sh.change_dir(dir);
+
+    // TODO: figure out tracing, tmp path (should be logged in some way)
+
+    for repo in repos.lines() {
+        let repo = repo?;
+
+        let context = Context { sh: &sh };
+        let Some((user, repo)) = repo.strip_prefix("github.com:").unwrap_or(&repo).rsplit_once('/') else {
+            anyhow::bail!("invalid repo: `{repo}` (expected github.com/user/repo)")
+        };
+        context.clone(user, repo)?;
+        let pr_url = {
+            let _guard = sh.push_dir(repo);
+            let _out = cmd!(sh, "sh -c").arg(cmd).read()?;
+            context.commit(&format!("`sh -c {}`", cmd), Some(id))?;
+            context.create_pr(Some(id), pr_info)?
+        };
+        println!("{}", pr_url);
+    }
+    Ok(())
+}
+
+struct Context<'a> {
+    sh: &'a Shell,
+}
+
+impl<'a> Context<'a> {
+    fn clone(&self, user: &str, repo: &str) -> Result<()> {
+        let dotgit_exists = self.sh.path_exists(self.sh.current_dir().join(repo).join(".git"));
+        if !dotgit_exists {
+            cmd!(self.sh, "git clone git@github.com:{user}/{repo}")
+                .quiet().ignore_stdout().ignore_stderr().run()?;
+        }
+
+        Ok(())
+    }
+
+    fn commit(&self, message: &str, branch: Option<&str>) -> Result<()> {
+        cmd!(self.sh, "git add --all").quiet().ignore_stdout().run()?;
+        cmd!(self.sh, "git --no-pager diff --cached --color=always")
+            .quiet().ignore_stdout().run()?;
+        match branch {
+            Some(branch) => {
+                cmd!(self.sh, "git switch -c {branch}").quiet().ignore_stdout().ignore_stderr().run()?;
+                cmd!(self.sh, "git commit -m {message}").quiet().ignore_stdout().run()?;
+                cmd!(self.sh, "git switch -").quiet().ignore_stdout().ignore_stderr().run()?;
+            }
+            None => cmd!(self.sh, "git commit -m {message}").quiet().ignore_stdout().run()?,
+        }
+        Ok(())
+    }
+
+    // TODO: support title and body
+    fn create_pr(&self, branch: Option<&str>, pr_info: Option<&PrInfo>) -> Result<String> {
+        if let Some(branch) = branch {
+            cmd!(self.sh, "git switch {branch}").quiet().ignore_stdout().ignore_stderr().run()?;
+        }
+        cmd!(self.sh, "git push --force-with-lease").quiet().ignore_stdout().ignore_stderr().run()?;
+
+        let pr_url = match cmd!(self.sh, "gh pr view --json url --jq '.url'").quiet().ignore_stderr().read() {
+            Ok(url) => url,
+            Err(_) => {
+                if let Some(PrInfo{title, body}) = pr_info {
+                    cmd!(self.sh, "gh pr create --title {title} --body {body}").quiet().read()?
+                } else {
+                    cmd!(self.sh, "gh pr create --fill-verbose").quiet().read()?
+                }
+            },
+        };
+
+        if branch.is_some() {
+            cmd!(self.sh, "git switch -").quiet().ignore_stdout().ignore_stderr().run()?;
+        }
+        Ok(pr_url)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod git;
-use clap::{Parser, Subcommand};
-use git::{list_recent_reviews, tidy_merged_go_mod};
+use clap::{Parser, Subcommand, Args};
+use clap_stdin::FileOrStdin;
+use std::io::BufReader;
+use git::{list_recent_reviews, tidy_merged_go_mod, poor_mans_refactorator};
+use petname::Generator;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -13,6 +16,38 @@ struct Cli {
 enum Commands {
     Git,
     GoModMerge,
+    Pmr(PoorMansRefactorator),
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct PoorMansRefactorator {
+    /// ID of this upsert "idea" (to enable re-using PRs)
+    #[arg(short, long)]
+    id: Option<String>,
+
+    #[clap(flatten)]
+    pr_info: Option<PrInfo>,
+
+    /// Command that will be invoked on each repo, with the resulting changes
+    /// applied in a commit + put for review
+    #[arg(required = true)]
+    command: String,
+
+    /// File from which to read, new-line separated
+    #[clap(default_value="-")]
+    repos_file: FileOrStdin,
+}
+
+#[derive(Args)]
+struct PrInfo {
+    /// Title to use for newly-created PRs
+    #[arg(short, long, required = true)]
+    title: String,
+
+    /// Body to use for newly-created PRs
+    #[arg(short, long, required = true)]
+    body: String,
 }
 
 fn main() {
@@ -21,5 +56,12 @@ fn main() {
     match &cli.command {
         Commands::Git => list_recent_reviews().expect("request should succeed"),
         Commands::GoModMerge => tidy_merged_go_mod().expect("couldn't tidy go.mod / go.sum"),
+        Commands::Pmr(PoorMansRefactorator{id, pr_info, command, repos_file}) => {
+            let id = id.to_owned().unwrap_or_else(|| {
+                petname::Petnames::default().generate_one(2, "-").expect("couldn't generate RNG name")
+            });
+            let reader = BufReader::new(repos_file.clone().into_reader().expect("failed to convert to reader"));
+            poor_mans_refactorator(&id, pr_info.as_ref(), command, reader).expect("couldn't create / update all PRs")
+        }
     }
 }
